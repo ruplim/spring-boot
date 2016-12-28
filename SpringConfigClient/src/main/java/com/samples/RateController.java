d66b93d41b71309ef06cd2e27fcd1799c291c161
ruplim <ruplimbora@gmail.com>
2016-12-28 16:25:42 -0600

    Updated

--- SpringConfigClient/src/main/java/com/samples/RateController.java
+++ SpringConfigClient/src/main/java/com/samples/RateController.java
@@ -1,11 +1,17 @@
 package com.samples;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// Refresh scope will refresh this bean. When we chnage value in github, the change value will be 
+// picked up by this bean we use hit the url localhost:8080/refresh.  The actuator library will
+// check for RefreshScope and refresh those beans
+
 @Controller
+@RefreshScope
 public class RateController {
 
 	@Value("${rate}")
